00f8621aca72383c9ec353b1b70d24e7c18afe50
Ritikarajwar <ritikarajwar0@gmail.com>
2024-08-11 00:22:31 +0530

    additional changes

--- firebaseauth.js
+++ firebaseauth.js
@@ -3,19 +3,20 @@
  import{getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"
  import{getFirestore , setDoc, doc} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"
 
- import { configDotenv } from "dotenv"
+//  import { configDotenv } from "dotenv"
 
-configDotenv()
+// configDotenv()
  
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  }
+    apiKey: "AIzaSyCI6iizKvywDB_omvXgITNuvixYY2k7f4U",
+    authDomain: "myapi-e1d9e.firebaseapp.com",
+    projectId: "myapi-e1d9e",
+    storageBucket: "myapi-e1d9e.appspot.com",
+    messagingSenderId: "825263078250",
+    appId: "1:825263078250:web:75b2528e558bf2edd4c313",
+    measurementId: "G-M6Z77DX185"
+  };
+ 
 
  const app = initializeApp(firebaseConfig)
  const analytics = getAnalytics(app)
